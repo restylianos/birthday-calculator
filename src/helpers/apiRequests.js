112ef33c1a6a976d181449da16ab91f1495cb3a1
stelios <restemstelios@yahoo.com>
2022-03-26 22:46:54 +0200

    remove unnecessary configs

--- src/helpers/apiRequests.js
+++ src/helpers/apiRequests.js
@@ -2,8 +2,5 @@ import { from } from 'rxjs';
 import axios from 'axios';
 
 export const getFacts = () => {
-  return axios.get('https://catfact.ninja/fact', {
-    'X-CSRF-TOKEN': 'OgE6m1ZYU9i1jb4etWXWHIlHrEl2vqeBqDOnZdz7',
-    accept: 'application/json',
-  });
+  return axios.get('https://catfact.ninja/fact');
 };
